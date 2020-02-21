dab0105cf770530d7d5c775d1f3b217e5be2ddaf
Oscar Zealley <oscar@zealley.com>
2020-02-21 14:39:22 +0000

    Added weather icons and UK/US zip code option

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 /* Data endpoint */
 
-//let journalData = [];
+// let journalData = [];
 
 let projectData = [
-  { location: 'UK',
+  { location: 'Edinburgh, GB',
     temperature: 9,
     date: '12.02.20',
-    content: 'Tempus, ipsum penatibus volutpat! Massa lacinia faucibus fusce hendrerit curabitur mus purus. Sed mauris litora augue penatibus? Quis natoque platea cras magnis leo. Nisl ligula lorem nec ipsum nascetur nostra bibendum.'
+    content: 'Tempus, ipsum penatibus volutpat! Massa lacinia faucibus fusce hendrerit curabitur mus purus. Sed mauris litora augue penatibus? Quis natoque platea cras magnis leo. Nisl ligula lorem nec ipsum nascetur nostra bibendum.',
+    iconCode: '04d'
   },
-  { location: 'France',
+  { location: 'New York, US',
     temperature: 13,
     date: '15.02.20',
-    content: 'Et eleifend magna habitant tortor egestas. Himenaeos risus magna ultrices tellus a ut, suscipit ullamcorper. Maecenas curae; penatibus dis turpis, inceptos suscipit fusce himenaeos cubilia habitant.'
+    content: 'Et eleifend magna habitant tortor egestas. Himenaeos risus magna ultrices tellus a ut, suscipit ullamcorper. Maecenas curae; penatibus dis turpis, inceptos suscipit fusce himenaeos cubilia habitant.',
+    iconCode: '01d'
   },
-  { location: 'Germany',
+  { location: 'Leicester, GB',
     temperature: 10,
     date: '18.02.20',
-    content: 'Tristique congue ridiculus purus magnis curae; faucibus venenatis neque dictum. Dignissim sem vel tempus inceptos pellentesque facilisi vestibulum. Rhoncus odio amet tincidunt conubia dolor quis massa.'
+    content: 'Tristique congue ridiculus purus magnis curae; faucibus venenatis neque dictum. Dignissim sem vel tempus inceptos pellentesque facilisi vestibulum. Rhoncus odio amet tincidunt conubia dolor quis massa.',
+    iconCode: '09d'
   },
 ];
 
@@ -42,11 +45,20 @@ const server = app.listen(port, ()=> console.log('Server running on localhost: '
 
 /* Routes */
 
+// Get all
 app.get('/all', (req, res) => {
   res.send(projectData);
 });
 
+// Get latest post
+app.get('/latest', (req, res) => {
+  latestPost = projectData[projectData.length - 1];
+  res.send(latestPost);
+});
+
+// Add post
 app.post('/add', (req, res) => {
   console.log(req.body);
   projectData.push(req.body);
+  res.send(req.body);
 })
--- website/app.js
+++ website/app.js
@@ -1,12 +1,12 @@
-window.onload = function() {
-  updateUI(true);
-}
-
+// Weather API url parts
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip='
 const apiKey = '&appid=435c250cfdd45953cf40e7b8134adbb6';
 
-// Get data
+// Weather icon url parts
+const iconUrlStart = 'http://openweathermap.org/img/wn/';
+const iconUrlEnd = '@2x.png';
 
+// Get data
 const retrieveData = async (url='') => {
   const request = await fetch(url);
   try {
@@ -19,8 +19,8 @@ const retrieveData = async (url='') => {
 }
 
 // Post data
-
 const postData = async (url = '', data ={}) => {
+  console.log('posting new data')
   const response = await fetch(url, {
     method: 'POST', 
     credentials: 'same-origin', 
@@ -32,87 +32,33 @@ const postData = async (url = '', data ={}) => {
 
   try {
     const newData = await response.json();
+    console.log('posted new data')
     return newData;
   } catch(error) {
     console.log(error);
   }
 }
 
-// Update UI to reflect data in server
-
-async function updateUI (firstLoad){
+// Initialise UI to reflect data in server
+const initialiseUI = async () => {
   const request = await fetch('http://localhost:8000/all');
   try {
-    console.log('Got data from server')
     const allData = await request.json();
-    if(firstLoad) {
-      console.log('First load so appending all entries')
-      allData.forEach(data => {
-        const content = document.querySelector('content');
-  
-        const newEntry = document.createElement('div');
-        newEntry.classList.add("weather-post");
-  
-        const location = document.createElement('p');
-        location.classList.add("alignleft");
-        location.classList.add("weather-location");
-        location.innerText = data.location;
-        newEntry.appendChild(location);
-  
-        const temperature = document.createElement('p');
-        temperature.classList.add("alignright");
-        temperature.classList.add("weather-temperature");
-        temperature.innerText = `${data.temperature}\xB0c`;
-        newEntry.appendChild(temperature);
-  
-        const date = document.createElement('p');
-        date.classList.add("weather-date");
-        date.innerText = data.date;
-        newEntry.appendChild(date);
-  
-        const text = document.createElement('p');
-        text.classList.add("weather-text");
-        text.innerText = data.content;
-        newEntry.appendChild(text);
-  
-        content.prepend(newEntry);
+    allData.forEach(data => buildJournalEntry(data));
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-      });
-      console.log('Appended all entries')
-    } else {
-      console.log('Not first load so appending last entry')
-      const lastEntry = allData.pop();
-      console.log('last entry : ' + lastEntry);
-      const content = document.querySelector('content');
+// Update UI with latest post
+const updateUi = async () => {
+  console.log('Updating UI');
 
-      const newEntry = document.createElement('div');
-      newEntry.classList.add("weather-post");
-
-      const location = document.createElement('p');
-      location.classList.add("alignleft");
-      location.classList.add("weather-location");
-      location.innerText = lastEntry.location;
-      newEntry.appendChild(location);
-
-      const temperature = document.createElement('p');
-      temperature.classList.add("alignright");
-      temperature.classList.add("weather-temperature");
-      temperature.innerText = `${lastEntry.temperature}\xB0c`;
-      newEntry.appendChild(temperature);
-
-      const date = document.createElement('p');
-      date.classList.add("weather-date");
-      date.innerText = lastEntry.date;
-      newEntry.appendChild(date);
-
-      const text = document.createElement('p');
-      text.classList.add("weather-text");
-      text.innerText = lastEntry.content;
-      newEntry.appendChild(text);
-
-      content.prepend(newEntry);
-      console.log('Appended last entry')
-    }
+  const request = await fetch('http://localhost:8000/latest');
+  try {
+    const latestPost = await request.json();
+    buildJournalEntry(latestPost);
+    console.log('updated UI')
   } catch (error) {
     console.log(error);
   }
@@ -122,33 +68,101 @@ const submitButton = document.getElementById('generate');
 submitButton.addEventListener('click', submitHandler);
 
 function submitHandler() {
-  console.log('Clicked submit')
-  const zipCode = event.target.previousElementSibling.previousElementSibling.value;
-  const text = event.target.previousElementSibling.value;
+  publishPost();
+}
 
-  const url = baseUrl + zipCode + apiKey;
+const publishPost = () => {
+  const zipCode = document.querySelector('#zip').value;
+  const text = document.querySelector('#feelings').value;
+  const countryCode = ',' + document.querySelector('#country').value;
+  const apiUrl = baseUrl + zipCode + countryCode + apiKey;
+
+  let today = dateString();
+
+  retrieveData(apiUrl)
+  .then( async function (data) {
+    const entry = {
+      location: data.name + ", " + data.sys.country,
+      temperature: Math.round(data.main.temp - 273.15),
+      date: today,
+      content: text,
+      iconCode: data.weather[0].icon
+    }
+    await postData('http://localhost:8000/add', entry);
+    updateUi();
+
+  });
+  
+}
+
+
+// Get the date in a string formatted dd/mm/yyyy
+function dateString() {
   let today = new Date();
   let dd = String(today.getDate()).padStart(2, '0');
   let mm = String(today.getMonth() + 1).padStart(2, '0');
   let yyyy = today.getFullYear();
-  today = dd + '/' + mm + '/' + yyyy;
-
-  retrieveData(url)
-  .then( data => {
-    console.log('Got data from open weather')
-    const entry = {
-      location: data.sys.country,
-      temperature: Math.round(data.main.temp - 273.15),
-      date: today,
-      content: text
-    }
-    console.log('Posting to server')
-    postData('http://localhost:8000/add', entry);
-    console.log('Posted to server')
-  })
-  .then(()=> {
-    console.log('Posted data to server')
-    updateUI(false);
-  });
+  let dateString = dd + '.' + mm + '.' + yyyy;
+  return dateString;
 }
 
+// Build a journal entry element from journal data
+function buildJournalEntry(data) {
+  // Build url to fetch icon from
+  iconUrl = iconUrlStart + data.iconCode + iconUrlEnd;
+
+  // Find content element
+  const content = document.querySelector('content');
+  
+  // Create journal entry card
+  const newEntry = document.createElement('div');
+  newEntry.classList.add("weather-post");
+
+  // Create post header div
+  const postHeader = document.createElement('div');
+  postHeader.classList.add("post-header");
+
+  // Create container for location and date
+  const locationDateContainer = document.createElement('div');
+
+  // Create location <p> and append to above container
+  const location = document.createElement('p');
+  location.classList.add("weather-location");
+  location.innerText = data.location;
+  locationDateContainer.appendChild(location);
+
+  // Create date <p> and append to above container
+  const date = document.createElement('p');
+  date.classList.add("weather-date");
+  date.innerText = data.date;
+  locationDateContainer.appendChild(date);
+
+  // Append location and date container to post header
+  postHeader.appendChild(locationDateContainer);
+
+  // Create and append icon to post header
+  const icon = document.createElement('img');
+  icon.setAttribute("src", iconUrl);
+  icon.setAttribute("alt", "Weather icon");
+  postHeader.appendChild(icon);
+
+  // Create and append temperature to post header
+  const temperature = document.createElement('p');
+  temperature.classList.add("weather-temperature");
+  temperature.innerText = `${data.temperature}\xB0c`;
+  postHeader.appendChild(temperature);
+
+  // Append post header to post
+  newEntry.appendChild(postHeader);
+
+  // Create and append journal text to post
+  const text = document.createElement('p');
+  text.classList.add("weather-text");
+  text.innerText = data.content;
+  newEntry.appendChild(text);
+
+  // Append journal entry to content
+  content.prepend(newEntry);
+}
+
+initialiseUI();
